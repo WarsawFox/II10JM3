6db13f516496f1ff848d60e0147f8e13b6c41c7b
WarsawFox <43025744+WarsawFox@users.noreply.github.com>
2018-09-07 13:10:08 +0200

    Add files via upload

---
+++ zad10.php
@@ -0,0 +1,30 @@
+<?php
+$dzień=rand(1,7);
+switch($dzień)
+{
+	case 1:
+	echo 'poniedziałęk';
+	break;
+	case 2:
+	echo 'wtorek';
+	break;
+	case 3:
+	echo 'środa';
+	break;
+	case 4:
+	echo 'czwartek';
+	break;
+	case 5:
+	echo 'piątek';
+	break;
+	case 6:
+	echo 'sobota';
+	break;
+	case 7:
+	echo 'niedziela';
+	break;
+}
+
+
+
+?>
---
+++ zad11.php
@@ -0,0 +1,15 @@
+
+<?php
+ 
+$przedmioty = array('pdg' => 'przygotowanie do prowadzenia działalności gospodarczej'
+   , 'wf' => 'wychowanie fizyczne'
+   , 'hist i społ' => 'historia i społeczeństwo'
+   , 'inf'  => 'informatyka');
+
+   foreach ($przedmioty as $klucz => $wartosc)
+		echo $klucz.' = '. $wartosc.'<br>';
+
+?>
+
+
+
