b8736987d539af76fc1c5583d9c7f8b028fb070d
WarsawFox <43025744+WarsawFox@users.noreply.github.com>
2018-09-19 10:53:21 +0200

    Add files via upload

---
+++ Lekcje 13 14 15/form_cw 08_inne_elementy/obsluga.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<title>Odebrane dane</title>
+<meta charset='UTF-8'>
+
+<?php
+
+var_dump( $_GET["ta1"] );
+
+var_dump( $_GET["s1"] );
+
+var_dump( $_GET["pole1"] );
+
+
+
+if( isset($_GET["cb1"] ) ){
+	var_dump( $_GET["cb1"] );	
+}
+
+if( isset($_GET["cb2"]) ){
+	var_dump( $_GET["cb2"] );
+}
+
+
+
+
+
+
+
+if( isset($_GET["plec"]) ){
+	var_dump( $_GET["plec"] );
+}
+
+
+
+if( isset($_GET["18lat"]) ){
+	var_dump( $_GET["18lat"] );
+}
+
+
+
+
+
+var_dump($_GET["ulubiony_kolor"]);
+
+
+
+
+var_dump($_GET["urodziny"]);
+
+
+
+
+
+
+
+if(isset($_GET["plik"])){
+	var_dump($_GET["plik"]);
+}
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
